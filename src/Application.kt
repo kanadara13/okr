f053f7469e116499961bb0880f7c607898cc4436
kanadara13 <jaehyeok.shin@leisureq.com>
2019-11-22 21:27:28 +0900

    unused main method remove

--- src/Application.kt
+++ src/Application.kt
@@ -10,9 +10,9 @@ import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) {
+/*fun main(args: Array<String>) {
     embeddedServer(Netty, commandLineEnvironment(args)).start()
-}
+}*/
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
